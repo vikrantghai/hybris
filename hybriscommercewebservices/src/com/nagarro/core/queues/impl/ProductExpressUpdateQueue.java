7817b8eeb22e8323ee55db70066582de613f1b36
Vikrant Ghai <vikrant.ghai@nagarro.com>
2016-04-12 16:42:46 +0530

    hybris commerce web services

---
+++ hybriscommercewebservices/src/com/nagarro/core/queues/impl/ProductExpressUpdateQueue.java
@@ -0,0 +1,25 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2015 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ *
+ *  
+ */
+package com.nagarro.core.queues.impl;
+
+import com.nagarro.core.queues.data.ProductExpressUpdateElementData;
+
+
+/**
+ * Queue for {@link com.nagarro.core.queues.data.ProductExpressUpdateElementData}
+ */
+public class ProductExpressUpdateQueue extends AbstractUpdateQueue<ProductExpressUpdateElementData>
+{
+	// EMPTY - just to instantiate bean
+}
